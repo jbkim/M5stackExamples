49f611cce046c7d396da1e1a31f5813364a86c84
Jinbuhm Kim <jinbuhm.kim@gmail.com>
2018-12-14 18:22:20 +0900

    update ThingSpeak code

--- ThinsSpeak/ThinsSpeak.ino
+++ ThinsSpeak/ThinsSpeak.ino
@@ -1,38 +1,69 @@
 #include <M5Stack.h>
 #include "DHT12.h"
 #include <Wire.h>     //The DHT12 uses I2C comunication.
-
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <Adafruit_NeoPixel.h>
+
+#define WIFI_TRIAL   10
+
+#define BUTTON_PIN   37    
+#define PIXEL_PIN    15    // Digital IO pin connected to the NeoPixels.
+
+#define PIXEL_COUNT 10
+
 
 DHT12 dht12;          //Preset scale CELSIUS and ID 0x5c.
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 // TODO: user config
-const char* ssid     = "........"; //Wifi SSID
-const char* password = "........"; //Wifi Password
-String apiKey = "........"; // API Key
+// const char* ssid     = "........"; //Wifi SSID
+// const char* password = "........"; //Wifi Password
+// String apiKey = "........"; // API Key
+
+const char* ssid     = "SmartAP"; //Wifi SSID
+const char* password = "20140701"; //Wifi Password
+String apiKey = "IUB6AOZRXA7NVIYV"; // API Key
 
 const char* host = "api.thingspeak.com";  // host
 const int httpPort = 80;      // port
 
+bool has_wifi = false;
+
 void setup() {
+  int trial = 0;
+
   M5.begin();
   Wire.begin();
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+  
 
   Serial.println("*****************************************************");
   Serial.println("Program Start : Connect ESP32 to Router.");
   Serial.println("Wait for WiFi... ");
+
+  M5.Lcd.setTextSize(2);
 
   // connecting to the WiFi network
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(500);
+    if (trial++ > WIFI_TRIAL) break;
   }
-  // connected
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());  
+
+  if (WiFi.status() == WL_CONNECTED) {
+    has_wifi = true;
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP()); 
+  }
+  else {
+    has_wifi = false;
+    Serial.println("NO WiFi");
+  }
+
 }
 
 void loop() {
@@ -42,21 +73,37 @@ void loop() {
   //Read temperature with preset scale.
   float temp = dht12.readTemperature();
   float humid = dht12.readHumidity();
-  Serial.print("\r\nTemperature: ");
-  M5.Lcd.print("Temperature: ");
-  Serial.print(temp);
-  M5.Lcd.print(temp);
 
-  //Read humidity.
+  // Debuf message
+  Serial.print("\r\nTemperature: ");
+  Serial.print(temp);
   Serial.print("*C  Humidity: ");
-  M5.Lcd.print("*C  Humidity: ");
   Serial.print(humid);
-  M5.Lcd.println(humid);
   Serial.println(" %RH");
+
+  // LCD Display
+  M5.Lcd.setTextColor(YELLOW);  
+  M5.Lcd.fillScreen(TFT_BLACK);  
+  M5.Lcd.setCursor(0, 0);
+  if (has_wifi) {
+    M5.Lcd.print("   WiFi: ");
+    M5.Lcd.println(WiFi.localIP());     
+   colorWipe(strip.Color(0, 255, 0), 0);  // Green
+  }else {
+    M5.Lcd.print(" No WiFi");    
+    colorWipe(strip.Color(255, 0, 0), 0);  // Red    
+  }
+  
+  M5.Lcd.setCursor(0, 80);
+  M5.Lcd.print("Temp: ");
+  M5.Lcd.print(temp);
+  M5.Lcd.print("C  Humid: ");
+  M5.Lcd.println(humid);
+
 
   if (!client.connect(host, httpPort)) {
     Serial.println("connection failed");
-    return;
+    // return;
   }else {
     String postStr = apiKey;
     postStr +="&field1=";
@@ -83,4 +130,13 @@ void loop() {
   client.stop();  // stop client
   
   delay(5000);
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
 }
